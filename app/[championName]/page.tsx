f3aee7f204684ebf09a05db15fb0cffba63c06fd
Juan Carlos Zepeda <juan97zepda@gmail.com>
2025-01-13 14:26:10 -0600

    fix splash art card size

--- app/[championName]/page.tsx
+++ app/[championName]/page.tsx
@@ -7,8 +7,7 @@ import { ChampionSkinImage } from "@/components/champion-skin-image";
 
 import { PageProps } from "@/.next/types/app/page";
 
-import {Image} from "@nextui-org/image";
-import NextImage from "next/image";
+import Image from "next/image";
 import { Card, CardHeader } from "@nextui-org/card";
 
 export default async function ChampionPage({ params }: PageProps) {
@@ -24,27 +23,26 @@ export default async function ChampionPage({ params }: PageProps) {
             <div>
                 <Link href="/" className="text-2xl font-bold">Back</Link>
             </div>
-            <Card isPressable={false} className="border-none" radius="lg">
-                <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <Card isPressable={false} className="">
+                <CardHeader className="absolute z-10 top-1 flex-col !items-start justify-start">    
                     <h2 className="text-white/60 text-4xl capitalize font-bold">{champion.title}</h2>
                     <h1 className="text-white text-7xl capitalize font-bold">{champion.name}</h1>
                 </CardHeader>
-            <div>
-                <div className="relative aspect-w-16 aspect-h-9">
-                    <Image
-                        id="champion-splash"
-                        style={{ width: "auto", height: "auto" }} 
-                        as={NextImage}
-                        priority={true}
-                        className="z-0 w-full h-full object-cover"
-                        src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`} 
-                        alt={`Picture of ${championName}`}
-                        width={1215}
-                        height={717}
-                     />
-                    <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"/>
+                <div className="w-full">
+                    <div className="w-full h-auto">
+                        <Image
+                            id="champion-splash"
+                            style={{ width: "100%", height: "100%" }} 
+                            priority={true}
+                            className="z-0 w-full h-full object-cover"
+                            src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`} 
+                            alt={`Picture of ${championName}`}
+                            width={1215}
+                            height={717}
+                        />
+                        <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"/>
+                    </div>
                 </div>
-            </div>
             </Card>
                 <div className=" py-10 grid grid-cols-4 gap-4 justify-center">
                 {skins.map((skin: any) => {
