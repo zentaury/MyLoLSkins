c32b468761259633bfb5acf7cc907f31670ac63d
Juan Carlos Zepeda <juan97zepda@gmail.com>
2026-01-20 21:42:10 -0600

    refactor: visuals of champions cards

--- components/champion-card.tsx
+++ components/champion-card.tsx
@@ -12,18 +12,16 @@ export function ChampionCard({ championKey, name, title, skinNumber, ownedCount 
     return (
         <Link href={championKey}>
             <Card isPressable isHoverable onPress={() => { }} className="border-none" radius="lg">
-                <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                    <div className="flex w-full justify-between items-start">
-                        <div>
-                            <p className="text-tiny text-white/60 uppercase font-bold">{title}</p>
-                            <h4 className="text-white font-medium text-large">{name}</h4>
-                        </div>
-                        {ownedCount !== undefined && ownedCount > 0 && (
-                            <Chip size="sm" color="warning" variant="solid" className="text-tiny font-bold">
+                <CardHeader className="absolute z-10 top-1 flex-col !items-start w-full">
+                    {ownedCount !== undefined && ownedCount > 0 && (
+                        <div className="absolute top-0 right-0 z-20 m-2">
+                            <Chip size="sm" color="warning" variant="solid" className="text-tiny font-bold shadow-lg">
                                 {ownedCount} OWNED
                             </Chip>
-                        )}
-                    </div>
+                        </div>
+                    )}
+                    <p className="text-tiny text-white/60 uppercase font-bold">{title}</p>
+                    <h4 className="text-white font-medium text-large">{name}</h4>
                 </CardHeader>
                 <Image
                     isZoomed
